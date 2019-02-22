2751a5135d3cb66c3f296e2408626afbac854c21
romeliyan <rkbliyanage@gmail.com>
2019-02-22 12:46:01 +0530

    T7 - variables in javascript

---
+++ T7.js
@@ -0,0 +1,22 @@
+//dont start a variable with a number
+
+//define variable
+var myVariable1;
+
+//initialize variable
+myVariable1 = 10;
+
+var myVariable2 = "Hello";
+
+//you can change the value of the variable or the type of the variable anytime
+//dynamic types - can change the type of the variable
+
+document.write("<br>" +myVariable1);
+document.write("<hr/>"+myVariable2);
+alert(myVariable2);
+
+document.write(myVariable2);
+document.write(myVariable2);
+myVariable2 = "world";
+document.write(myVariable2);
+document.write(myVariable2);
