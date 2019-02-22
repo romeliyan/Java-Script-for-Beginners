633aa97bc05aeb4712514813d94ec78826b50e34
romeliyan <rkbliyanage@gmail.com>
2019-02-22 14:37:19 +0530

    T8 - basic mathematical operations

---
+++ T8.js
@@ -0,0 +1,10 @@
+var myVar1 = 10;
+var myVar2 = "Hello";
+
+var myVar3 = myVar1 + myVar2;  //types are different so they concatenate
+
+alert(myVar3);
+
+var myVar4 = myVar1 * myVar2;  // types are different therefore cannot multiply
+
+document.write(myVar4);    //result will be NaN
