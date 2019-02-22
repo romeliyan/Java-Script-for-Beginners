f8e783afeac75c208fbd67a54c781785adbc8607
romeliyan <rkbliyanage@gmail.com>
2019-02-22 17:40:27 +0530

    if else in javascript

---
+++ T12.js
@@ -0,0 +1,21 @@
+var youLikeMeat = true;
+var youLikeVegitables = false;
+
+if (youLikeMeat){
+    document.write("Here is the Meat menu...");
+}
+
+if(youLikeVegitables){
+    document.write("Here is the Vegetarian Menu... ")
+}
+
+if (7 < 0){
+    document.write("Hello");
+}
+
+if(youLikeMeat){
+    document.write("You like me");
+}
+else {
+    document.write("You hate me");
+}
