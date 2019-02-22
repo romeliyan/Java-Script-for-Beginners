a24610cb0ae335615ef8c56f31e8a06b33effefb
romeliyan <rkbliyanage@gmail.com>
2019-02-22 12:17:43 +0530

    T6 - Basics of  javascript

---
+++ T6.js
@@ -0,0 +1,9 @@
+//java script is case sensitive
+alert("Hello world"); //built in function
+//Alert("Hello");  //this would not work
+
+//not sensitive to line breaks or white spaces
+
+//each statement ends with a semicolon
+
+//Java Script runs from top to bottom
