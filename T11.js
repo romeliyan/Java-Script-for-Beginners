f9cffb1e40a2b252e6d9086fc0ba51a57997e349
romeliyan <rkbliyanage@gmail.com>
2019-02-22 17:06:36 +0530

    T11 - boolean in javascript

---
+++ T11.js
@@ -0,0 +1,15 @@
+//booleans
+
+var isLikeMeat = true;
+
+// if we type in console
+// 7 > 5 ; true
+// 7 = 7 ; error
+// 7 == 7 ; true
+
+// 7 == 5 ;false
+
+Boolean(7 > 5); //true
+Boolean(6); //true because a number
+Boolean("Hello"); //true
+Boolean(""); //false
