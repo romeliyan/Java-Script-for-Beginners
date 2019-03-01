42794fdd3eea81f96708fab37b8a1db63b330bcd
romeliyan <rkbliyanage@gmail.com>
2019-03-01 11:45:11 +0530

    T25 - string split and string slice

---
+++ T25.js
@@ -0,0 +1,11 @@
+var str = "Hello world";
+var str2 = str.slice(2,9);
+document.write(str);
+document.write(str2);
+
+
+var tags = "one , two , three, four ,five, six";
+var tagsArray = tags.split(",");
+document.write(tags);
+document.write(tagsArray);
+
