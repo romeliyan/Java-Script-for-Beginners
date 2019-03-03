68759ae445310d36ec7c965412d78487d83dd084
romeliyan <rkbliyanage@gmail.com>
2019-03-03 20:44:31 +0530

    T27 - objects in javascript

---
+++ T27.js
@@ -0,0 +1,22 @@
+//Objects
+//is a container which encloses data and behaviours togather
+//objects can be strings , numbers, arrays ..
+//can extract properties and behaviours(functions/methods)
+
+// var myCar = new Car();
+
+// myCar.topSpeed;
+// myCar.drive();
+
+var myString = new String();
+myString = "Hello";
+
+myString.length;
+myString.toLowerCase();
+
+var myString2 = "hi";
+
+myString2.length;
+myString2.toLowerCase();
+
+
